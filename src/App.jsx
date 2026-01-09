a8e0ce61740c3bcd79f595934a1e856dd32dda39
NSC <sumaiya.afroza.99@gmail.com>
2026-01-09 14:21:39 +0600

    tag feature with filter

--- src/App.jsx
+++ src/App.jsx
@@ -67,23 +67,3 @@ export const App = () => {
   )
 }
 
-
-// import React from 'react'
-// import {Header} from './components/header.jsx';
-// import {HeroSection} from './components/heroSection.jsx';
-// import {Footer} from './components/footer.jsx';
-// import TaskBoard from './components/taskBoard.jsx';
-//
-// export const App = () => {
-//   return (
-//     <>
-//       <div className='bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50'>
-//         <Header/>
-//         <HeroSection/>
-//         <TaskBoard/>
-//       </div>
-//
-//       <Footer/>
-//     </>
-//   )
-// }
